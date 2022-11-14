add8ec72ea78b8dfe25da464e968b7b0fa78c716
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-14 21:04:38 +0700

    add TextEditor Details data

--- app/src/main/java/com/dicoding/text_editor/TextEditorData.kt
+++ app/src/main/java/com/dicoding/text_editor/TextEditorData.kt
@@ -17,7 +17,7 @@ object TextEditorData {
         "Visual Studio Code"
     )
 
-    private val heroesImages = intArrayOf(
+    private val textEditorImages = intArrayOf(
         R.drawable.adobe_dreamweaver,
         R.drawable.atom,
         R.drawable.bluefish,
@@ -31,5 +31,15 @@ object TextEditorData {
         R.drawable.ultraedit,
         R.drawable.vim,
         R.drawable.visualstudiocode
+    )
+
+    private val textEditorDetails = arrayOf(
+        "Adobe Dreamweaver adalah aplikasi desain dan pengembangan web yang menyediakan editor WYSIWYG visual (bahasa sehari-hari yang disebut sebagai Design view) dan kode editor dengan fitur standar seperti syntax highlighting, code completion, dan code collapsing serta fitur lebih canggih seperti real-time syntax checking dan code introspection untuk menghasilkan petunjuk kode untuk membantu pengguna dalam menulis kode.",
+        "Atom is a free and open-source text and source code editor for macOS, Linux, and Microsoft Windows with support for plug-ins written in JavaScript, and embedded Git Control. Developed by GitHub, Atom is a desktop application built using web technologies. Atom was initially written in CoffeeScript and Less, but much of it has been converted to JavaScript.",
+        "Bluefish is a free and open-source software advanced text editor with a variety of tools for programming and website development. It supports coding languages including HTML, XHTML, CSS, XML, PHP, C, C++, JavaScript, Java, Go, Vala, Ada, D, SQL, Perl, ColdFusion, JSP, Python, Ruby, and shell. It is available for many platforms, including Linux, macOS and Windows, and can be used via integration with GNOME or run as a stand-alone application. Designed as a compromise between plain text editors and full programming IDEs, Bluefish is lightweight, fast and easy to learn, while providing many IDE features. It has been translated into 17 languages.",
+        "Brackets is a source code editor with a primary focus on web development. Created by Adobe Inc., it is free and open-source software licensed under the MIT License, and is currently maintained on GitHub by open-source developers. It is written in JavaScript, HTML and CSS. Brackets is cross-platform, available for macOS, Windows, and most Linux distributions. The main purpose of Brackets is its live HTML, CSS and JavaScript editing functionality.",
+        "Emacs adalah sekelompok editor teks yang memiliki berbagai fungsi dan dikenal luas oleh kalangan pemrogram komputer dan pengguna komputer lainnya. Versi aslinya ditulis oleh Richard Stallman pada tahun 1975, pada awalnya bersama Guy L. Steele, Jr.. Namanya diambil dari singkatan Editor MACroS. Sejak itu banyak versi Emacs yang telah muncul, tetapi saat ini yang paling banyak dipakai adalah GNU Emacs (juga ditulis oleh Stallman) dan XEmacs.",
+        "Geany is a free and open-source lightweight GUI text editor using Scintilla and GTK, including basic IDE features. It is designed to have short load times, with limited dependency on separate packages or external libraries on Linux. It has been ported to a wide range of operating systems, such as BSD, Linux, macOS, Solaris and Windows. The Windows port lacks an embedded terminal window; also missing from the Windows version are the external development tools present under Unix, unless installed separately by the user. Among the supported programming languages and markup languages are C, C++, C#, Java, JavaScript, PHP, HTML, LaTeX, CSS, Python, Perl, Ruby, Pascal, Haskell, Erlang, Vala and many others.",
+        ""
     )
 }
