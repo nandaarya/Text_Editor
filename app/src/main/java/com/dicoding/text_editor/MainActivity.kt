256907403bc4211ffcfbbb038748a06fc15c7f1a
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-16 21:58:50 +0700

    apply recyclerView in activity_main

--- app/src/main/java/com/dicoding/text_editor/MainActivity.kt
+++ app/src/main/java/com/dicoding/text_editor/MainActivity.kt
@@ -2,10 +2,16 @@ package com.dicoding.text_editor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvTextEditors: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvTextEditors = findViewById(R.id.rv_heroes)
+        rvTextEditors.setHasFixedSize(true)
     }
 }
