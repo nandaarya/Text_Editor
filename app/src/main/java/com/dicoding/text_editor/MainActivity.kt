02cbb574bc1325ee362d662cf7b053ead6dd7edb
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-17 20:17:33 +0700

    apply grid to options in mainactivity

--- app/src/main/java/com/dicoding/text_editor/MainActivity.kt
+++ app/src/main/java/com/dicoding/text_editor/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -28,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         rvTextEditors.adapter = listTextEditorAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvTextEditors.layoutManager = GridLayoutManager (this, 2)
+        val gridTextEditorAdapter = GridTextEditorAdapter(list)
+        rvTextEditors.adapter = gridTextEditorAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
@@ -41,12 +48,15 @@ class MainActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
+                showRecyclerList()
             }
 
             R.id.action_grid -> {
+                showRecyclerGrid()
             }
 
             R.id.action_cardview -> {
+
             }
         }
     }
