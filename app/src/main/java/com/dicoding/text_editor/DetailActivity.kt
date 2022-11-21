186e4bcccbfeb2a6ca46caf255b2698de3c8433c
Nanda Arya Putra <nandaarya404@gmail.com>
2022-11-21 22:43:34 +0700

    add intent for share

--- app/src/main/java/com/dicoding/text_editor/DetailActivity.kt
+++ app/src/main/java/com/dicoding/text_editor/DetailActivity.kt
@@ -1,14 +1,18 @@
 package com.dicoding.text_editor
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var title: String
+    private lateinit var detail: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -22,5 +26,27 @@ class DetailActivity : AppCompatActivity() {
         tvName.text = intent.getStringExtra("text_editor_name")
         tvDetail.text = intent.getStringExtra("text_editor_detail")
         imgPhoto.setImageResource(intent.getIntExtra("text_editor_img", 0))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_share -> {
+                title = intent.getStringExtra("text_editor_name")!!
+                detail = intent.getStringExtra("text_editor_detail")!!
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "Text Editor $title. $detail")
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
